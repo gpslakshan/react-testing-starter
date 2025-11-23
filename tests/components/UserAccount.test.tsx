608cc8cca1a4c0d977a4e2ff52c80f76af64810a
Sachin <sachinlakshan04@gmail.com>
2025-11-23 18:03:18 +0530

    exercise: test user account

---
+++ tests/components/UserAccount.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen } from "@testing-library/react";
+import UserAccount from "../../src/components/UserAccount";
+import { User } from "../../src/entities";
+
+describe("UserAccount", () => {
+  it("should render user name", () => {
+    const user: User = { id: 1, name: "Sachin" };
+
+    render(<UserAccount user={user} />);
+
+    expect(screen.getByText(user.name)).toBeInTheDocument();
+  });
+
+  it("should render edit button if user is an admin", () => {
+    const user: User = { id: 1, name: "Sachin", isAdmin: true };
+
+    render(<UserAccount user={user} />);
+
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+    expect(button).toHaveTextContent(/edit/i);
+  });
+
+  it("should not render edit button if user is not an admin", () => {
+    const user: User = { id: 1, name: "Sachin" };
+
+    render(<UserAccount user={user} />);
+
+    const button = screen.queryByRole("button");
+    expect(button).not.toBeInTheDocument();
+  });
+});
